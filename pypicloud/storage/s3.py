30acd50b3a553de142007430e49fa65d48ef6969
zzugg <zzugg@enswer.net>
2014-07-08 21:08:47 +0900

    s3 storage with cloudfront-support

--- pypicloud/storage/s3.py
+++ pypicloud/storage/s3.py
@@ -1,4 +1,5 @@
 """ Store packages in S3 """
+import os
 import logging
 import time
 from contextlib import contextmanager
@@ -12,6 +13,8 @@ import boto
 import posixpath
 from .base import IStorage
 from boto.s3.key import Key
+from boto.cloudfront import CloudFrontConnection
+from boto.cloudfront.distribution import Distribution
 from pypicloud.models import Package
 from pypicloud.util import parse_filename, getdefaults
 
@@ -26,6 +29,7 @@ class S3Storage(IStorage):
 
     def __init__(self, request=None, bucket=None, expire_after=None,
                  buffer_time=None, bucket_prefix=None, prepend_hash=None,
+                 use_cloudfront=False, cf_distribution=None, cf_keypair_id=None, cf_private_key=None, cf_url=None,
                  **kwargs):
         super(S3Storage, self).__init__(request, **kwargs)
         self.bucket = bucket
@@ -33,6 +37,11 @@ class S3Storage(IStorage):
         self.buffer_time = buffer_time
         self.bucket_prefix = bucket_prefix
         self.prepend_hash = prepend_hash
+        self.use_cloudfront = use_cloudfront
+        self.cf_distribution = cf_distribution
+        self.cf_keypair_id = cf_keypair_id
+        self.cf_private_key = cf_private_key
+        self.cf_url = cf_url
 
     @classmethod
     def configure(cls, settings):
@@ -64,6 +73,22 @@ class S3Storage(IStorage):
                                    boto.s3.connection.Location.DEFAULT)
             bucket = s3conn.create_bucket(aws_bucket, location=location)
         kwargs['bucket'] = bucket
+
+        kwargs['use_cloudfront'] = asbool(settings.get('storage.cloudfront', False))
+        if kwargs['use_cloudfront']:
+            dist_id = settings.get('storage.cloudfront_distribution_id', None)
+            cfrontconn = CloudFrontConnection(access_key, secret_key)
+            dist_config = cfrontconn.get_distribution_config(dist_id)
+            dist_info = cfrontconn.get_distribution_info(dist_id)
+            distribution = Distribution(connection=cfrontconn,
+                                        config=dist_config,
+                                        domain_name=dist_info.domain_name,
+                                        id=dist_id)
+            kwargs['cf_distribution'] = distribution
+            kwargs['cf_keypair_id'] = settings.get('storage.cloudfront_keypair_id', None)
+            kwargs['cf_private_key'] = settings.get('storage.cloudfront_private_key', None)
+            kwargs['cf_url'] = settings.get('storage.cloudfront_url', None)
+
         return kwargs
 
     def get_path(self, package):
@@ -107,14 +132,23 @@ class S3Storage(IStorage):
         expire = package.data.get('expire', 0)
         changed = False
         if 'url' not in package.data or time.time() > expire:
-            key = Key(self.bucket)
-            key.key = self.get_path(package)
-            expire_after = time.time() + self.expire_after
-            url = key.generate_url(expire_after, expires_in_absolute=True)
+            if self.use_cloudfront:
+                expire_after = int(time.time()) + self.expire_after
+                unsigned_url = os.path.join(self.cf_url, self.get_path(package))
+                url = self.cf_distribution.create_signed_url(unsigned_url,
+                                                             self.cf_keypair_id,
+                                                             expire_time=expire_after,
+                                                             private_key_file=self.cf_private_key)
+            else:
+                key = Key(self.bucket)
+                key.key = self.get_path(package)
+                expire_after = time.time() + self.expire_after
+                url = key.generate_url(expire_after, expires_in_absolute=True)
             package.data['url'] = url
             expire = expire_after - self.buffer_time
             package.data['expire'] = expire
             changed = True
+
         return package.data['url'], changed
 
     def download_response(self, package):
